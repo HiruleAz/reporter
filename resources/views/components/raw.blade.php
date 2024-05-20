44206044100592f8c3996f696e15bcb6eafaf818
HiruleAz <hiruleh@hotmail.com>
2024-05-20 17:42:20 +0800

    update

--- resources/views/components/raw.blade.php
+++ resources/views/components/raw.blade.php
@@ -1,24 +1,31 @@
-<div id="raw" class="tabcontent border-collapse border border-slate-400">
-<table class="table-auto">
+<div id="raw" class="tabcontent w-1/2  content-center flex justify-center items-center">
+<table class="table-auto border-collapse border-2 border-slate-400">
   <thead>
+    <tr class="border-2 border-slate-300">
+      <th class="border-2 border-slate-300">Sample</th>
+      <th class="border-2 border-slate-300">Date & Time</th>
+      <th class="border-2 border-slate-300">CEMS</th>
+      <th class="border-2 border-slate-300">SRM</th>
+    </tr>
     <tr>
-      <th class="border border-slate-300">Song</th>
-      <th class="border border-slate-300">Artist</th>
-      <th class="border border-slate-300">Year</th>
+    <th class="border-2 border-slate-300">Sample</th>
+      <th class="border-2 border-slate-300">Date & Time</th>
+      <th class="border-2 border-slate-300">CEMS</th>
+      <th class="border-2 border-slate-300">SRM</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+  <tbody class="border-2 border-slate-300">
+    <tr class="border-2 border-slate-300">
+      <td class="border-2 border-slate-300">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
       <td>Malcolm Lockyer</td>
       <td>1961</td>
     </tr>
-    <tr>
+    <tr class="border-2 border-slate-300">
       <td>Witchy Woman</td>
       <td>The Eagles</td>
       <td>1972</td>
     </tr>
-    <tr>
+    <tr class="border-2 border-slate-300">
       <td>Shining Star</td>
       <td>Earth, Wind, and Fire</td>
       <td>1975</td>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,31 @@
   @vite('resources/css/app.css')
 </head>
 <body>
+<x-sidebar>
+ 
+ </x-sidebar>
 <x-tabselect>
 </x-tabselect>
+<form class="max-w-sm mx-auto flex">
+  <label for="underline_select" class="sr-only">Underline select</label>
+  <select id="underline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+      <option value="NOx" selected>NOx</option>
+      <option value="CO">CO</option>
+      <option value="SO2">SO2</option>
+  </select>
+  <label for="overline_select" class="sr-only">Underline select</label>
+  <select id="overline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+      <option value="Raw">Raw</option>
+      <option value="Standardised">Standardised</option>
+      <option value="FR">France</option>
+      <option value="DE">Germany</option>
+  </select>
+</form>
+<div>
+
 <x-raw>
   
 </x-raw>
-<x-sidebar>
-</x-sidebar>
-
+</div>
 </body>
 </html>
