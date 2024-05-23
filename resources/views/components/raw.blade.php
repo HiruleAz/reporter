fc5b054a3217d2c12b37c10ddf191e1f1389d5ed
HiruleAz <hiruleh@hotmail.com>
2024-05-23 17:46:00 +0800

    change

--- resources/views/components/raw.blade.php
+++ resources/views/components/raw.blade.php
@@ -1,8 +1,8 @@
-<div id="raw" class="tabcontent w-1/2 mx-auto content-center flex justify-center items-center">
-  <div colspan={3} class="border-2 row-span-2 border-slate-300">Sample</>
-  <th class="border-2 border-slate-300">Date & Time</th>
-  <th rowspan={3} class="border-2 row-span-3 border-slate-300">CEMS</th>
-  <th class="border-2 border-slate-300">SRM</th>
+<!-- <div id="raw" class="tabcontent w-1/2 mx-auto content-center flex justify-center items-center">
+  <div colspan={5} class="border-2 w-auto row-span-2 border-slate-300">Sample</>
+  <div class="border-2 border-slate-300">Date & Time</div>
+  <div rowspan={3} class="border-2 row-span-3 border-slate-300">CEMS</div>
+  <div class="border-2 border-slate-300">SRM</div>
   <tr>
     <th class="border-2 border-slate-300">Test</th>
     <th class="border-2 border-slate-300">(xi)</th>
@@ -41,6 +41,284 @@
     </tr>
   </tbody>
 </div>
-</div>
+</div> -->
 
 <!-- <link rel="stylesheet" href="https://stackoverflow.com/questions/72955073/how-to-group-table-rows-in-html-css-tailwind"> -->
+
+<div class="m-4 grid grid-cols-9 bg-gray-50 border border-1 mx-auto">
+  <!-- table head starts -->
+  <div class="row-span-3 flex items-center justify-center border-[1px]">Sample</div>
+  <div class="row-span-2 col-span-2 flex items-center justify-center border-[1px] ">Date & Time</div>
+  <div class="col-span-3 flex items-center justify-center border-[1px] ">CEMS</div>
+  <div class="col-span-3 flex items-center justify-center border-[1px] ">SRM</div>
+
+  <div class="flex items-center justify-center border-[1px] ">(xi)</div>
+  <div class="flex items-center justify-center border-[1px] ">O2</div>
+  <div class="flex items-center justify-center border-[1px] ">H2O</div>
+  <div class="flex items-center justify-center border-[1px] ">(yi)</div>
+  <div class="flex items-center justify-center border-[1px] ">O2</div>
+  <div class="flex items-center justify-center border-[1px] ">H2O</div>
+
+  <div class="flex items-center justify-center border-[1px] ">Start</div>
+  <div class="flex items-center justify-center border-[1px] ">End</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+<!-- table head ends -->
+
+  <!-- table body starts -->
+  <div class="flex items-center justify-center border-[1px] ">1</div>
+  <div class="row-span-3 col-span-2 flex items-center justify-center border-[1px]">Zero Values</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+
+  <div class="flex items-center justify-center border-[1px] ">2</div>
+  <div class="flex items-center justify-center border-[1px]">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px]">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+
+  <div class="flex items-center justify-center border-[1px] ">3</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+
+  <!-- Linearity Data starts here -->
+
+  <div class="flex items-center justify-center border-[1px] ">not used</div>
+  <div class="row-span-5 col-span-2 flex items-center justify-center border-[1px]">Linearity Data (mg/mg3)</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px]">N/A</div>
+  <div class="flex items-center justify-center border-[1px]">N/A</div>
+
+  <div class="flex items-center justify-center border-[1px] ">not used</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+
+  <div class="flex items-center justify-center border-[1px] ">not used</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+
+  <div class="flex items-center justify-center border-[1px] ">not used</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px]">N/A</div>
+
+  <div class="flex items-center justify-center border-[1px] ">not used</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">0.00</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+  <div class="flex items-center justify-center border-[1px] ">N/A</div>
+
+    <!-- Where the rest of data starts -->
+
+  <div class="flex items-center justify-center border-[1px] ">4</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">5</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">6</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px]">7</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">8</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">9</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">10</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">11</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">12</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">13</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">14</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">15</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">16</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">17</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">18</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">19</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">20</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+  <div class="flex items-center justify-center border-[1px] ">21</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 08:00</div>
+  <div class="flex items-center justify-center border-[1px] ">02.12.2021 9:30</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">mg/m3</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+  <div class="flex items-center justify-center border-[1px] ">%</div>
+
+    <!-- table body ends -->
+</div>
